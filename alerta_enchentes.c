00607ec56b28c4b393fab2f866ffc261dea44edc
ateniltonjr <ateniltonjr09@gmail.com>
2025-05-18 02:20:41 -0300

    finalizado

--- alerta_enchentes.c
+++ alerta_enchentes.c
@@ -1,27 +1,29 @@
 #include "tarefas_utilizadas.h"
 #include "lib/buttons.h"
 
-int main() {  
-    controle(PINO_MATRIZ);
-    iniciar_buzzer();
-    iniciar_rgb(); 
-    init_display();
+int main() 
+{  
+    controle(PINO_MATRIZ); // Inicializa a matriz de leds
+    iniciar_botoes();     // Inicializa o botão B
+    iniciar_buzzer();    // Inicializa o buzzer A
+    stdio_init_all();   // inicializa o monitor serial
+    init_display();    // Inicializa o display
+    iniciar_rgb();    // Inicializa o led vermelho
+    
+    gpio_set_irq_enabled_with_callback(botaoB, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+
     ssd1306_fill(&ssd, !cor);
     escrever(&ssd, "INICIANDO!!!", 10, 10, cor);
     sleep_ms(1000);
     ssd1306_fill(&ssd, !cor);
 
-    iniciar_botoes();
-    gpio_set_irq_enabled_with_callback(botaoB, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
-
-    stdio_init_all();
     sleep_ms(1000);
 
-    // Create queues
+    // queues criadas
     xQueueJoyX = xQueueCreate(10, sizeof(uint16_t));
     xQueueJoyY = xQueueCreate(10, sizeof(uint16_t));
 
-    // Create tasks
+    // tasks criadas
     xTaskCreate(vJoy_y_Task, "Joystick Y", 256, NULL, 1, NULL);
     xTaskCreate(vJoy_x_task, "Joystick X", 256, NULL, 1, NULL);
     xTaskCreate(vLedBlueTask, "LED Blue", 256, NULL, 1, NULL);
